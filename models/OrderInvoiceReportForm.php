ee3ce00b8d3de3f521e4c72e3e28eab0a51d81a0
La Jayuhni Yarsyah <lajayuhni@gmail.com>
2015-03-27 08:31:59 +0700

    Invoice Report Form

--- models/OrderInvoiceReportForm.php
+++ models/OrderInvoiceReportForm.php
@@ -22,6 +22,30 @@ class OrderInvoiceReportForm extends Model
     {
         return [
             [['date_from','date_to'], 'required'],
+            [['date_from','date_to'],'safe'],
+            [['sales'],'checkSales'],
         ];
+    }
+
+    public function checkSales($attribute,$params){
+        // var_dump($salesId);
+        foreach($this->$attribute as $salesId){
+            // validate must be a number
+            $validator = new \yii\validators\NumberValidator();
+            if($validator->validate($salesId,$error)){
+                $query = new \yii\db\Query;
+                $query->select('id')
+                    ->from(ResUsers::tableName())
+                    ->where('id = :salesIds')
+                    ->addParams([':salesIds'=>$salesId]);
+                if(!$query->exists()){
+                    $this->addError($attribute,'Sales Man did you search is not exist for sales man id '.$salesId);
+                }
+            }else{
+                $this->addError($attribute,'Sales Man did u search is not valid');
+            }
+            
+
+        }
     }
 }
