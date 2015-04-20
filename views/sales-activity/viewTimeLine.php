cb0797b130dc32b15c5bf60c4b3ec02bdeede6ad
La Jayuhni Yarsyah <lajayuhni@gmail.com>
2015-04-20 10:31:44 +0700

    commit this morning

--- views/sales-activity/viewTimeLine.php
+++ views/sales-activity/viewTimeLine.php
@@ -4,10 +4,13 @@ use nirvana\infinitescroll\InfiniteScrollPager;
 use yii\widgets\Pjax;
 use yii\bootstrap\Modal;
 use yii\bootstrap\Tabs;
+use yii\bootstrap\BootstrapAsset;
 ?>
 
 <?php
-View::registerCssFile('@web/css/bootsnip-timeline.css');
+// View::registerCssFile('@web/css/bootsnip-timeline.css');
+View::registerCssFile('@web/css/moTimeline.css',['depends'=>[BootstrapAsset::className()]]);
+
 
 echo Tabs::widget([
 	// 'renderTabContent'=>false,
@@ -15,7 +18,8 @@ echo Tabs::widget([
         [
             'label'		=> '<span class="glyphicon glyphicon-tasks"></span>',
             'encode'	=> false,
-            'content'	=> $this->render('timeline',['dataProvider'=>$dataProvider]),
+            // 'content'   => $this->render('timeline',['dataProvider'=>$dataProvider]),
+            'content'   => $this->render('timeline2',['dataProvider'=>$dataProvider]),
         ],
         [
             'label'		=> '<span class="glyphicon glyphicon-stats"></span>',
